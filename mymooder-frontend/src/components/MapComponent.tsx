c8f6b6d05ea8818d3dd981fffb8fe43fc58975e1
Kim Sundeen <geoinformatica.consulting@gmail.com>
2024-06-29 15:49:29 -0500

    Shifted map.tsx to use MapComponent.tsx instead

--- mymooder-frontend/src/components/MapComponent.tsx
+++ mymooder-frontend/src/components/MapComponent.tsx
@@ -1,29 +1,23 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { LatLng, LeafletView } from 'react-native-leaflet-view';
+import { LatLng, LeafletView } from 'react-native-leaflet-view-2';
 
-
-export function MapComponent(dimensions: any) {
+export function MapComponent() {
   const DEFAULT_COORDINATE: LatLng = {
     lat: 43.233224,
     lng: -89.346395,
   };
   return (
-    <View>
-        <LeafletView
-            mapMarkers={[
-                {
-                    position: DEFAULT_COORDINATE,
-                    icon: '📍',
-                    size: [32, 32],
-                },
-            ]}
-            mapCenterPosition={DEFAULT_COORDINATE}
-        />
-    </View>
+      <LeafletView
+          mapMarkers={[
+              {
+                  position: DEFAULT_COORDINATE,
+                  icon: '📍',
+                  size: [32, 32],
+              },
+          ]}
+          mapCenterPosition={DEFAULT_COORDINATE}
+      />
   );
 }
-
-const styles = StyleSheet.create({});
 
 export default MapComponent;
